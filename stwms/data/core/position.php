2969829df12390aea034804ffd40eba38d31743f
springwind2006 <spring.wind2006@163.com>
2016-04-06 17:31:37 +0800

    修复冲突

--- stwms/data/core/position.php
+++ stwms/data/core/position.php
@@ -1,7 +1,3 @@
 <!--<?php die(0);?>-->
 <?xml version="1.0" encoding="UTF-8"?>
-<<<<<<< HEAD
 <root><position fd="id|name|model|catid|content|maxnum|isreplace|listorder" autoid="4"/></root>
-=======
-<root><position fd="id|name|model|catid|content|maxnum|isreplace|listorder" autoid="4"><it><v0>1</v0><v1>首页推荐商家</v1><v2>shops</v2><v3>4</v3><v4>shops.111,shops.89,shops.163,shops.164,shops.178,shops.184,shops.166,shops.158,shops.186,shops.129</v4><v5>12</v5><v6>1</v6><v7>0</v7></it><it><v0>2</v0><v1>推荐职位</v1><v2>recruitings</v2><v3></v3><v4>recruitings.8,recruitings.15,recruitings.22,recruitings.24,recruitings.20,recruitings.11,recruitings.13,recruitings.14,recruitings.18,recruitings.23,recruitings.25</v4><v5>12</v5><v6>1</v6><v7>0</v7></it><it><v0>3</v0><v1>推荐求职</v1><v2>jobwantedinfoes</v2><v3/><v4>jobwantedinfoes.11,jobwantedinfoes.3,jobwantedinfoes.5,jobwantedinfoes.8,jobwantedinfoes.10,jobwantedinfoes.12</v4><v5>12</v5><v6>1</v6><v7>0</v7></it><it><v0>4</v0><v1>打折推荐</v1><v2>discounts</v2><v3>1</v3><v4>discounts.43,discounts.51,discounts.54,discounts.57,discounts.61</v4><v5>10</v5><v6>1</v6><v7>0</v7></it></position></root>
->>>>>>> branch 'master' of https://github.com/springwind2006/stwms.git
