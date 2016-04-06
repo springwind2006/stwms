67d73820e98c94df5947a235679cd1fe92fed6a4
springwind2006 <spring.wind2006@163.com>
2016-04-06 16:59:22 +0800

    初始化数据

--- stwms/config/system.cfg.php
+++ stwms/config/system.cfg.php
@@ -1,14 +1,14 @@
 <?php
 return array(
 //常规设置
-  'version'=>'stwms v2.0',
+  'version'=>'stwms v3.0',
   'charset' => 'utf-8', //网站字符集
 	'timezone' => 'Etc/GMT-8', //网站时区（只对php 5.1以上版本有效），Etc/GMT-8 实际表示的是 GMT+8
 	'lang' => 'zh-cn',  //网站语言包	
 	'debug' => 0, //是否显示调试信息
 	'lock_ex' => 1, //写入缓存时是否建立文件互斥锁定（如果使用nfs建议关闭）
 
-  'db_conn' => 'mysql', //系统启用的数据库连接配置，数据库连接在database.cfg.php文件中配置
+  'db_conn' => 'sqlite3', //系统启用的数据库连接配置，数据库连接在database.cfg.php文件中配置
   'attachment_stat' => 1, //附件状态使用情况统计
 	'admin_log' => 0, //是否记录后台操作日志
 	'errorlog' => 0, //1、保存错误日志到 cache/error_log.php | 0、在页面直接显示
@@ -21,9 +21,9 @@ return array(
 
 
 //界面风格配置
-  'style' => 'default',//管理系统风格
+  'style' => 'blue',//管理系统风格
   'template' => 'daoke',//前台模板名称，位于template/目录
-  'static' => 'daoke/',//前台访问静态文件路径，如果为相对路径则相对于STATIC_URL/styles
+  'static' => 'html/',//前台访问静态文件路径，如果为相对路径则相对于STATIC_URL/styles
   'html_root' => 'dk/',//静态文件生成路径，相对于网站根目录
   
 //Session配置
--- stwms/data/core/classify.php
+++ stwms/data/core/classify.php
@@ -1,3 +1,3 @@
 <!--<?php die(0);?>-->
 <?xml version="1.0" encoding="UTF-8"?>
-<root><classify fd="id|name|describe|catids|level|listorder" autoid="5"><it><v0>1</v0><v1>道客新闻</v1><v2>道客官方发布的新闻分类</v2><v3>11</v3><v4>1</v4><v5>0</v5></it><it><v0>2</v0><v1>道客商家</v1><v2>入驻道客商家或淘宝街店铺分类</v2><v3>22,4</v3><v4>1</v4><v5>0</v5></it><it><v0>3</v0><v1>求职招聘</v1><v2>求职和招聘信息职位分类 </v2><v3>7,8</v3><v4>2</v4><v5>0</v5></it><it><v0>4</v0><v1>求购租赁二手</v1><v2>求购租赁二手等信息分类</v2><v3>5</v3><v4>3</v4><v5>0</v5></it><it><v0>5</v0><v1>商品信息</v1><v2>打折优惠、商城和一号街商品信息分类</v2><v3>1,2,3</v3><v4>3</v4><v5>0</v5></it></classify></root>
+<root><classify fd="id|name|describe|catids|level|listorder" autoid="5"/></root>
