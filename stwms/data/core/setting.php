ca410d4c314892d87e6cc1b1c50f0f69b5d35232
springwind2006 <spring.wind2006@163.com>
2016-04-06 17:31:37 +0800

    修复冲突

--- stwms/data/core/setting.php
+++ stwms/data/core/setting.php
@@ -1,6 +1,5 @@
 <!--<?php die(0);?>-->
 <?xml version="1.0" encoding="UTF-8"?>
-<<<<<<< HEAD
 <root><setting fd="id|key|value"><it><v0>1</v0><v1>web</v1><v2>array (  'name' =&gt; '',  'url' =&gt; '',  'icp' =&gt; '',  'statistics' =&gt; '',  'tel' =&gt; '',  'email' =&gt; '',  'addr' =&gt; '',  'qq' =&gt; '',  'app' =&gt; '1',  'status' =&gt; '1',  'notice' =&gt; '网站维护中......',)</v2></it><it><v0>2</v0><v1>seo</v1><v2>array (  'title' =&gt; '',  'keywords' =&gt; '',  'description' =&gt; '',)</v2></it><it><v0>3</v0><v1>attachment</v1><v2>array (  'upload_num' =&gt; '50',  'size' =&gt; '2048',  'type' =&gt; 'gif|jpg|jpeg|png|bmp|swf|flv|doc|ppt|xls|docx|pptx|xlsx|txt|rar|zip|wma|mp3|wav|wmw|avi|mpg|mpeg',  'md5_check' =&gt; '1',  'upload_maxwidth' =&gt; '1000',  'upload_maxheight' =&gt; '2000',  'w_type' =&gt; '0',  'w_minwidth' =&gt; '300',  'w_minheight' =&gt; '300',  'w_img' =&gt; 'mark.png',  'w_color' =&gt; '#666666',  'w_text' =&gt; 'www.dk198.cn   ',  'w_fontsize' =&gt; '12',  'w_pct' =&gt; '85',  'w_quality' =&gt; '80',  'w_pos' =&gt; '9',)</v2></it><it><v0>4</v0><v1>mail</v1><v2>array (
   'type' =&gt; '1',
   'server' =&gt; '',
@@ -18,39 +17,3 @@
   'qq_appkey' =&gt; '',
   'qq_callback' =&gt; '',
 )</v2></it></setting></root>
-=======
-<root><setting fd="id|key|value"><it><v0>1</v0><v1>web</v1><v2>array (
-  'name' =&gt; '道客网',
-  'url' =&gt; 'http://www.dk198.cn',
-  'icp' =&gt; '滇ICP备14004283号-1',
-  'statistics' =&gt; '&lt;script src="http://s95.cnzz.com/stat.php?id=4756481&amp;web_id=4756481" language="JavaScript"&gt;&lt;/script&gt; ',
-  'tel' =&gt; '0877-2016992,13099889198',
-  'email' =&gt; 'china-dr@china-daoran.com',
-  'addr' =&gt; '中国.云南.玉溪.时代广场9B11',
-  'qq' =&gt; '85181227，22376363',
-  'app' =&gt; '1',
-  'status' =&gt; '1',
-  'notice' =&gt; '网站维护中......',
-)</v2></it><it><v0>2</v0><v1>seo</v1><v2>array (
-  'title' =&gt; '道客网——为道客谋福利',
-  'keywords' =&gt; '玉溪 商家 道客 联盟 道商 道士 道文化',
-  'description' =&gt; '道客网 道客联盟，商务信息服务 ',
-)</v2></it><it><v0>3</v0><v1>attachment</v1><v2>array (
-  'upload_num' =&gt; '50',
-  'size' =&gt; '2048',
-  'type' =&gt; 'gif|jpg|jpeg|png|bmp|swf|flv|doc|ppt|xls|docx|pptx|xlsx|txt|rar|zip|wma|mp3|wav|wmw|avi|mpg|mpeg',
-  'md5_check' =&gt; '1',
-  'upload_maxwidth' =&gt; '1000',
-  'upload_maxheight' =&gt; '2000',
-  'w_type' =&gt; '0',
-  'w_minwidth' =&gt; '300',
-  'w_minheight' =&gt; '300',
-  'w_img' =&gt; 'mark.png',
-  'w_color' =&gt; '#666666',
-  'w_text' =&gt; 'www.dk198.cn   ',
-  'w_fontsize' =&gt; '12',
-  'w_pct' =&gt; '85',
-  'w_quality' =&gt; '80',
-  'w_pos' =&gt; '9',
-)</v2></it><it><v0>4</v0><v1>mail</v1><v2>array (  'type' =&gt; '1',  'server' =&gt; '',  'port' =&gt; '25',  'from' =&gt; '',  'auth' =&gt; '1',  'user' =&gt; '',  'password' =&gt; '',)</v2></it><it><v0>5</v0><v1>connect</v1><v2>array (  'sina_akey' =&gt; '',  'sina_skey' =&gt; '',  'qq_akey' =&gt; '',  'qq_skey' =&gt; '',  'qq_appid' =&gt; '',  'qq_appkey' =&gt; '',  'qq_callback' =&gt; '',)</v2></it></setting></root>
->>>>>>> branch 'master' of https://github.com/springwind2006/stwms.git
